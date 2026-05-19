90ca5e911734332e057ea82766fea879542f86fa
Roland Koffi <koffiroland02@icloud.com>
2026-05-19 17:07:32 +0200

    lc. 392. Is Subsequence

---
+++ Two Pointers/ Is_Subsequence.py
@@ -0,0 +1,10 @@
+def isSubsequence(s: str, t: str) -> bool:
+    i = 0
+    j = 0
+    count = 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            count += 1
+            i += 1
+        j += 1
+    return count == len(s)
